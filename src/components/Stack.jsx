262ff7835be6a5baf50e84a019339ae6d9410553
Danie Antón <danielantcam@gmail.com>
2026-02-06 20:23:39 +0100

    Fixed inconsistent icon brightness in stack section

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,14 +1,15 @@
+import clsx from 'clsx';
 import { SiHtml5, SiCss3, SiTailwindcss, SiJavascript, SiReact, SiNextdotjs, SiNodedotjs, SiExpress, SiPostgresql } from 'react-icons/si';
 
 const STACK = [
-  { name: "HTML", Svg: SiHtml5, color: "#f26629" },
+  { name: "HTML", Svg: SiHtml5, color: "#f26629", classes: "opacity-90" },
   { name: "CSS", Svg: SiCss3, color: "#2466f2" },
-  { name: "Tailwind", Svg: SiTailwindcss, color: "#38BDF8" },
-  { name: "Javascript", Svg: SiJavascript, color: "#F7DF1E" },
-  { name: "React", Svg: SiReact, color: "#61DAFB" },
-  { name: "Next.js", Svg: SiNextdotjs, color: "#FFFFFF" },
+  { name: "Tailwind", Svg: SiTailwindcss, color: "#38BDF8", classes: "opacity-70" },
+  { name: "Javascript", Svg: SiJavascript, color: "#F7DF1E", classes: "opacity-50" },
+  { name: "React", Svg: SiReact, color: "#61DAFB", classes: "opacity-60" },
+  { name: "Next.js", Svg: SiNextdotjs, color: "#FFFFFF", classes: "opacity-40" }, // Property "classes" allows me to correct the difference in color between      icons by adding the necessary tailwind classes
   { name: "Node.js", Svg: SiNodedotjs, color: "#339933" },
-  { name: "Express", Svg: SiExpress, color: "#FFFFFF" },
+  { name: "Express", Svg: SiExpress, color: "#FFFFFF", classes: "opacity-50" },
   { name: "PostgreSQL", Svg: SiPostgresql, color: "#336791" },
 ];
 
@@ -25,7 +26,7 @@ export default function Stack() {
   );
 }
 
-function StackElement({ name, Svg, color }) {
+function StackElement({ name, Svg, color, classes }) {
   return (
     <div 
       className="group relative flex flex-col items-center justify-center p-6 rounded-2xl border-2 border-white/5 bg-white/2 transition-all duration-500 hover:bg-white/5 hover:-translate-y-1"
@@ -37,7 +38,10 @@ function StackElement({ name, Svg, color }) {
       />
       
       <Svg 
-        className="size-10 mb-3 transition-all duration-500 grayscale group-hover:grayscale-0" 
+        className={clsx(
+          "size-10 mb-3 transition-all duration-500 grayscale group-hover:grayscale-0",
+          classes
+        )}
         style={{ color: color }} 
       />
       
